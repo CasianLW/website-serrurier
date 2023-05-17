e9a59afa8839e8cf5a9b957683a0c7a2182bc1e5
CasianLW <ciorbcasian@gmail.com>
2023-05-18 00:46:04 +0200

    appy layout to _app / all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import Head from "next/head";
+import LayoutComponent from "@/layout";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <LayoutComponent>
+        <Component {...pageProps} />
+      </LayoutComponent>
+    </>
+  );
+};
+
+export default App;
