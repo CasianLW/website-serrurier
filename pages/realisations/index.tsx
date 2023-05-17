5e1555f1b68b2973de91278bccfb7e14437ece67
CasianLW <ciorbcasian@gmail.com>
2023-05-18 01:39:06 +0200

    set correct active navlink

--- pages/realisations/index.tsx
+++ pages/realisations/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function Realisations() {
   const { setActiveNavLink } = useNavSettingsContext();
   useEffect(() => {
-    setActiveNavLink(NavLinkEnum.Home);
+    setActiveNavLink(NavLinkEnum.Realisations);
   }, [setActiveNavLink]);
   return (
     <>
