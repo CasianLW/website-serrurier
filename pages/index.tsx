bbe86eb6fee93322767d0137dbbffd9940d3ff8a
CasianLW <ciorbcasian@gmail.com>
2023-05-18 00:54:57 +0200

    imported nav settings/context to set active page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,15 @@ import {
   UrgenceComponent,
   ContactComponent,
 } from "@/components/";
+import { NavLinkEnum } from "@/components/nav";
+import { useNavSettingsContext } from "@/context/nav-settings-context";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { setActiveNavLink } = useNavSettingsContext();
+  useEffect(() => {
+    setActiveNavLink(NavLinkEnum.Home);
+  }, [setActiveNavLink]);
   return (
     <>
       <HeroComponent />
