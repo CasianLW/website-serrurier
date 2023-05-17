cf894c0b1bc6148cc42480a341beac453634077b
CasianLW <ciorbcasian@gmail.com>
2023-05-18 00:42:41 +0200

    empty footer component

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const FooterComponent: FC = () => {
+  return (
+    <section>
+      Footer
+      <div>Partie avec Menu / Contact / Reseaux</div>
+      <div>Partie avec Zones</div>
+    </section>
+  );
+};
+
+export default FooterComponent;
