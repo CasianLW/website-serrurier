474138d81f4a6d580fbe78bce8473370bed59f92
CasianLW <ciorbcasian@gmail.com>
2023-05-18 00:46:25 +0200

    seo component for SEO Manipulation on every page

---
+++ components/shared/seo-component.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import Head from "next/head";
+
+interface Seo {
+  title: string;
+  description: string;
+}
+interface Props {
+  seo: Seo;
+}
+
+const SeoComponent: FC<Props> = ({ seo }) => (
+  <Head>
+    <title>{seo.title}</title>
+    <meta name="description" content={seo.description} />
+    <meta property="og:title" content={seo.title} />
+    <meta property="og:description" content={seo.description} />
+  </Head>
+);
+
+export default SeoComponent;
