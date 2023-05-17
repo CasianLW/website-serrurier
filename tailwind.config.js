4eacc3154cd510fcbfc71f7e0a96eafbc324f53a
CasianLW <ciorbcasian@gmail.com>
2023-05-18 01:38:29 +0200

    certiclefs colors for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,7 +13,23 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "certiclefs-blue": {
+          400: "#3C8490",
+          500: "#205776",
+          600: "#173156",
+        },
+        "certiclefs-white": {
+          100: "#ffffff",
+          300: "#949494",
+        },
+        "certiclefs-black": {
+          100: "#363636",
+          300: "#1C1B1F",
+          600: "#000000",
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
 };
