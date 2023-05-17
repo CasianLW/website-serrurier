775a36f3596de20f8669ee3f4a742a7cb6a43401
CasianLW <ciorbcasian@gmail.com>
2023-05-18 00:58:10 +0200

    integration SEO realisations

--- pages/realisations/index.tsx
+++ pages/realisations/index.tsx
@@ -1,4 +1,5 @@
 import { NavLinkEnum } from "@/components/nav";
+import SeoComponent from "@/components/shared/seo-component";
 import { useNavSettingsContext } from "@/context/nav-settings-context";
 import { useEffect } from "react";
 
@@ -9,6 +10,13 @@ export default function Realisations() {
   }, [setActiveNavLink]);
   return (
     <>
+      <SeoComponent
+        seo={{
+          title: "Réalisations du serrurier Certiclefs ",
+          description: "Description SEO de la page",
+        }}
+        // plus tard tu pense a ajouter une image au seo cmsa elle apparait dns les recherches google
+      />
       <header className="mt-24">Introduction page / hero</header>
       <main>Realisations, design a definir</main>
     </>
