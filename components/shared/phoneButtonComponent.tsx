668afc94f21468f2869476beb7b6f99e366ea5b5
CasianLW <ciorbcasian@gmail.com>
2023-05-18 02:12:35 +0200

    composant phone, used in all website

---
+++ components/shared/phoneButtonComponent.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import Image from "next/image";
+import { phoneIcon } from "@/assets/icons";
+
+interface BtnProp {
+  supplentaryClasses?: string;
+  phoneNumber?: string;
+}
+
+const PhoneButton: FC<BtnProp> = ({
+  supplentaryClasses,
+  phoneNumber = "0924451275",
+}) => {
+  return (
+    <a
+      href={`tel:${phoneNumber}`}
+      className={
+        "grid grid-cols-3 py-1 px-2 contact-btn md:py-1 md:px-2 pr-4 md:pr-6 " +
+        ` ${supplentaryClasses}`
+      }
+    >
+      <Image
+        src={phoneIcon}
+        className="m-auto max-w-[20px]"
+        alt="call phone button"
+        width={44}
+        height={44}
+        // blurDataURL="data:..." automatically provided
+      />
+      <div className="text-xs col-span-2 md:text-base">
+        <p className="font-bold">0945131478</p>
+        <p>Disponible</p>
+      </div>
+    </a>
+  );
+};
+
+export default PhoneButton;
